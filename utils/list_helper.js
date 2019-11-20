e3acf45ac7b07337c890b4b210a191855a9db2e9
pankaj-pant <pankajpant89@gmail.com>
2019-11-20 10:51:15 +0200

    4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,20 @@ const dummy = (blogs) => {
   return 1
 }
 
+
+const totalLikes = (blogs) => {
+  const initialValue = 0
+  const sum = blogs.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.likes
+    , initialValue
+  )
+
+  return blogs.length === 0
+    ? 0 :
+    sum
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
